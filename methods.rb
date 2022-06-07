2fa166a84f0dd101a74e24ed846ab83fad29f802
Daryl Rogony <darylrogony@gmail.com>
2022-06-07 07:10:33 +0300

    commit tree e378a3740d3f92d7beee3f46e482dfb076fe9c42

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+
+def greet ( name = "Naureen!")
+  puts "Hello, #{name}!"
+end
+greet "Jimmy!"
+
+def greet_with_default( name = "programmer")
+  puts "Hello, #{name}!"
+end
+greet_with_default "Naureen!"
+
+#add two numbers
+def add num1, num2
+  num1 + num2
+end
+
+#halve
+def halve(number = 6) 
+  if number.class != Integer
+    return nil
+  end
+
+  number / 2
+end
